d3d4dca267ebb0cac28d44a8c5d0a4ff8e69e5c8
Splash UAT & Production User <splash@instantiated.ca>
2010-10-05 23:45:00 -0400

    Change 2010 to 2011 everywhere.

--- templates/splash2011/index.php
+++ templates/splash2011/index.php
@@ -3,8 +3,8 @@
  * @copyright	Copyright (C) 2005 - 2008 Open Source Matters. All rights reserved.
  * @license		GNU/GPL, see LICENSE.php
  * Joomla! is free software. This version may have been modified pursuant
- * to the GNU General <?php echo $this->baseurl ?>/templates/splash2010 License, and as distributed it includes or
- * is derivative of works licensed under the GNU General <?php echo $this->baseurl ?>/templates/splash2010 License or
+ * to the GNU General <?php echo $this->baseurl ?>/templates/splash2011 License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General <?php echo $this->baseurl ?>/templates/splash2011 License or
  * other free or open source software licenses.
  * See COPYRIGHT.php for copyright notices and details.
  */
@@ -18,38 +18,38 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 <head>
 
 <jdoc:include type="head" />
-<link href="<?php echo $this->baseurl ?>/templates/splash2010/images/favicon.ico" rel="shortcut icon" type="image/x-icon" />
-<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/splash2010/css/main.css" type="text/css" />
+<link href="<?php echo $this->baseurl ?>/templates/splash2011/images/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/splash2011/css/main.css" type="text/css" />
 
 <!--[if lt IE 7]>
 <style type="text/css" media="screen">
-BODY { behavior: url("<?php echo $this->baseurl ?>/templates/splash2010/css/csshover.htc"); }
+BODY { behavior: url("<?php echo $this->baseurl ?>/templates/splash2011/css/csshover.htc"); }
 .header  {
 	background: none;
 	height: 152px;
-	filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src='<?php echo $this->baseurl ?>/templates/splash2010/images/bg/header-bg.png', sizingMethod='scope');
+	filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src='<?php echo $this->baseurl ?>/templates/splash2011/images/bg/header-bg.png', sizingMethod='scope');
 }
 .header A,
 .header .right-header,
 .news A, .box A {position: relative;}
 .small-box-top {
 	background: none;
-	filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src='<?php echo $this->baseurl ?>/templates/splash2010/images/bg/small-box-top.png', sizingMethod='scope');
+	filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src='<?php echo $this->baseurl ?>/templates/splash2011/images/bg/small-box-top.png', sizingMethod='scope');
 }
 .small-box-bottom {
 	background: none;
-	filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src='<?php echo $this->baseurl ?>/templates/splash2010/images/bg/small-box-bottom.png', sizingMethod='scope');
+	filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src='<?php echo $this->baseurl ?>/templates/splash2011/images/bg/small-box-bottom.png', sizingMethod='scope');
 }
 .boxes-top {
 	background: none;
-	filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src='<?php echo $this->baseurl ?>/templates/splash2010/images/bg/boxes-top.png', sizingMethod='scope');
+	filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src='<?php echo $this->baseurl ?>/templates/splash2011/images/bg/boxes-top.png', sizingMethod='scope');
 }
 .boxes-bottom {
 	background: none;
-	filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src='<?php echo $this->baseurl ?>/templates/splash2010/images/bg/boxes-bottom.png', sizingMethod='scope');
+	filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src='<?php echo $this->baseurl ?>/templates/splash2011/images/bg/boxes-bottom.png', sizingMethod='scope');
 }
 .content UL LI {
-	background: url("<?php echo $this->baseurl ?>/templates/splash2010/images/icons/list.gif") no-repeat left 13px;
+	background: url("<?php echo $this->baseurl ?>/templates/splash2011/images/icons/list.gif") no-repeat left 13px;
 	border: 1px oslid red;
 }
 .header BUTTON {margin-top: 3px;}
@@ -85,9 +85,9 @@ BODY { behavior: url("<?php echo $this->baseurl ?>/templates/splash2010/css/cssh
 
     			<div class="relax">&nbsp;</div>
     			<jdoc:include type="modules" name="sociable" />
-    			<span>A Conference Facility &middot; Portland Oregon, USA</span>
+    			<span>Hilton Portland &amp; Executive Tower &middot; Portland Oregon, USA</span>
     		</div>
-    		<a href="<?php echo $this->baseurl ?>"><img src="<?php echo $this->baseurl ?>/templates/splash2010/images/logotype2011-2.png" alt="Splash" /></a>
+    		<a href="<?php echo $this->baseurl ?>"><img src="<?php echo $this->baseurl ?>/templates/splash2011/images/logotype2011-2.png" alt="Splash" /></a>
 			<jdoc:include type="modules" name="main-menu" />
     	</div>
     	
